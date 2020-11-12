7d7bb1ecf67baac167307c55bcd066f346ab37ff
Richard Decal <richard.decal@ncf.edu>
2020-11-12 19:51:05 -0300

    update firefox keys

--- apps/firefox/firefox.py
+++ apps/firefox/firefox.py
@@ -1,4 +1,4 @@
-from talon import Context, Module, actions, app, clip, ctrl, ui
+from talon import Context, Module, actions, app
 
 ctx = Context()
 mod = Module()
@@ -14,15 +14,22 @@ app: firefox
 
 @ctx.action_class("user")
 class user_actions:
+    """
+    Defines actions for Firefox.
+
+    See default keybindings here:
+    https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-quickly
+    """
+
     def tab_jump(number: int):
         if number < 9:
             if app.platform == "mac":
                 actions.key("cmd-{}".format(number))
             else:
-                actions.key("ctrl-{}".format(number))
+                actions.key("alt-{}".format(number))
 
     def tab_final():
         if app.platform == "mac":
             actions.key("cmd-9")
         else:
-            actions.key("ctrl-9")
+            actions.key("alt-9")
